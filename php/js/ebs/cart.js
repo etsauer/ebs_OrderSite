d683a69d5fad3d64d3f978283cf470e23b8b0f39
Eric Sauer <etsauer@gmail.com>
2013-07-17 10:20:51 -0500

    Adding Cart feature

---
+++ php/js/ebs/cart.js
@@ -0,0 +1,83 @@
+define([ "dojo/dom", "dojo/json", "dojo/cookie", "dojo/_base/array" ],
+		function(dom, json, cookie, array) {
+
+			return {
+				load : function(config) {
+					var cart = this.get(config);
+					var cartDiv = dom.byId(config.cart.domWrapper);
+					console.log("Shopping Cart: " + cart);
+					cartDiv.innerHTML = "<p>Shopping Cart</p>"; 
+					cartDiv.innerHTML += this.getHTML(cart);
+				},
+				
+				get : function(config) {
+					return json.parse(cookie(config.cart.cookieName));
+				},
+
+				getHTML : function(cart) {
+					var html = '';
+					html += '<ul>';
+
+					array.forEach(cart.items, function(item) {
+						html += '<li>' + item.qty + "x " + item.productName
+								+ " " + item.height + " x " + item.width
+								+ " ... $" + item.subTotal + '</li>';
+					});
+
+					html += '</ul>';
+					return html;
+				},
+				
+				update : function(config, cart) {
+					cookie(config.cart.cookieName, json.stringify(cart), {
+						expires : 5
+					});
+				},
+				
+				add : function(config, item) {
+					var cart = this.init(config);
+					cart.items.push(item);
+					this.update(config, cart);
+					this.load(config);
+				},
+				
+				init : function(config) {
+					var cart = this.get(config);
+					if (cart) {
+						return cart;
+					}
+					cart = {};
+					cart.items = [];
+					cart.subTotal = 0;
+					cart.shipping = 0;
+					cart.totalPrice = 0;
+					return cart;
+				},
+
+				putSampleCart : function(config) {
+					var cart = {};
+					cart.items = [];
+					var item1 = {
+						productName : "Custom Window",
+						height : 12.5,
+						width : 15,
+						qty : 8,
+						unitPrice : 24.38,
+						subTotal : 195.04
+					};
+					var item2 = {
+						productName : "Custom Window",
+						height : 38,
+						width : 46,
+						qty : 2,
+						unitPrice : 227.24,
+						subTotal : 454.48
+					};
+					cart.items.push(item1);
+					cart.items.push(item2);
+					cart.shipping = 0;
+					cart.totalPrice = 649.52;
+					this.update(config, cart);
+				}
+			};
+		});
---
+++ php/js/ebs/conf.js
@@ -0,0 +1,17 @@
+define([ "dojo/json", "dojo/request/xhr" ], function(json, xhr) {
+	return {
+		get : function(file, callback) {
+			// Create config object from json config file
+			xhr(file, {
+				handleAs : "json"
+			}).then(function(data) {
+//				console.log(data);
+				callback(data);
+			}, function(err) {
+				console.error(err);
+			}, function(evt) {
+				console.log(evt);
+			});
+		}
+	}
+});
